6732486ae295078dde83d7b52f4264ab06285723
chandu1026 <chanducjchandan@gmail.com>
2026-09-12 18:44:42 +0530

    Correct syntax and string operations in program1.3.cpp
    Fix syntax errors and improve string handling in the program.

--- program1.3.cpp
+++ program1.3.cpp
@@ -2,4 +2,14 @@
 #include<string>
 #include<cctype>
 usig namespace std;
-
+int main()
+string S ="verifiction";
+cout<<"firts 4 :"<<S.substr(0.4)<<endl;
+cout<<"from 4 :"<<S.substr(4)<<endl;
+int C=S compare ("verify");
+cout<<"compare Vs' verify:"<<((unsigned char)ch))freq[tolower(ch)-'a']++;
+cout<<"letter counts:";
+for(int i=0;i<26;++i)if (freq[i]) cout<<char('a'+i)<<":"<<freq[i]<<" ";
+cout<<endl;
+return 0;
+}
